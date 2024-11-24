bb3f2988044b2d58e808e1aef58fc20a8485db74
JRamonHA <hdezagjr@gmail.com>
2024-11-23 21:31:00 -0600

    Final commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
-from shiny.express import input, render, ui
+from shiny.express import input, ui
+from shinywidgets import render_widget
 
 f = './data/Tmx.csv'
 tmx = pd.read_csv(f, index_col=0, parse_dates=True)
@@ -9,10 +10,10 @@ columnas = list(tmx.columns)
 ui.page_opts(title="Explorador de archivos")
 
 with ui.sidebar():
-    ui.input_select("var", "Selecciona una variable", choices=columnas)
-    ui.input_switch("month", "Promedio mensual", value=True)
+        ui.input_select("var", "Selecciona una variable", choices=columnas)
+        ui.input_switch("month", "Promedio mensual", value=True)  
 
-@render.ui
+@render_widget
 def grafica():
     var = input.var()
     df = tmx.resample('ME').mean().reset_index() if input.month() else tmx.reset_index()
@@ -23,4 +24,4 @@ def grafica():
         title=f"{var}",
         labels={'time': 'Fecha', var: var}
     )
-    return ui.HTML(fig.to_html(full_html=False, include_plotlyjs='cdn'))
+    return fig
